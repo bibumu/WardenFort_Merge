104ec05a19681cf5daccf08d93678e2575580cb2
bibumu <161674166+bibumu@users.noreply.github.com>
2024-03-20 12:46:39 +0800

    memory exception-fixd

--- WardenFort/WardenFort/wardenfort.cpp
+++ WardenFort/WardenFort/wardenfort.cpp
@@ -1,14 +1,105 @@
-#include "wardenfort.h"
+#include "wardenfort.h"  
 #include "ui_wardenfort.h"
+#include <pcap.h>
+#include <tchar.h>
+#include <Winsock2.h>
+#include <QThread>
+#include <functional>
+
 
 wardenfort::wardenfort(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::wardenfort)
 {
     ui->setupUi(this);
+    listModel = new QStringListModel(this);
+    // Set the model for the listView
+    ui->listPacket->setModel(listModel);
+    set_LineEdit_Text();
+    ui->listPacket->setModel(listModel);
+    initialize_Devices();
+    // Initialize the QStringListModel
+    
+    // Set up the combo box
+    connect(ui->listBox, QOverload<int>::of(&QComboBox::activated), this, &wardenfort::on_listBox_currentIndexChanged);
+
+    // Connect the returnPressed signal of the QLineEdit to the on_LineEdit_ReturnPressed() slot
+    connect(ui->lineEdit, &QLineEdit::returnPressed, this, &wardenfort::on_LineEdit_ReturnPressed);
 }
 
 wardenfort::~wardenfort()
 {
     delete ui;
+}
+
+void wardenfort::set_LineEdit_Text() {
+    // Assuming your QLineEdit widget has an object name "lineEdit"
+    ui->lineEdit->setText(""); // Set the text of the line edit widget
+}
+
+void wardenfort::on_LineEdit_ReturnPressed()
+{
+    // This function will be called when the return key is pressed in the QLineEdit
+    QString text = ui->lineEdit->text();
+    // Do whatever you want with the text, for example:
+    qDebug() << "Text entered: " << text;
+    updateList(text);
+    set_LineEdit_Text();
+
+}
+
+void wardenfort::on_listBox_currentIndexChanged(int index) {
+    // Ensure index is valid
+    if (index < 0 || index >= ui->listBox->count())
+        return;
+
+    // Jump to the selected adapter
+    pcap_if_t* d = alldevs;
+    for (int i = 0; i < index; i++) {
+        if (d == nullptr)
+            return; // Error handling
+        d = d->next;
+    }
+
+    // Now d points to the selected adapter, you can perform further operations with it
+    qDebug() << "Selected adapter: " << d->name;
+    
+
+}
+
+void wardenfort::initialize_Devices() {
+    char errbuf[PCAP_ERRBUF_SIZE]; // Buffer to store error messages
+    if (pcap_findalldevs(&alldevs, errbuf) == -1) {
+        QTextStream out(stdout);
+        out << "Error in pcap_findalldevs: " << errbuf << "\n";
+        out.flush();
+        return;
+    }
+
+    // Add devices to comboBox
+    for (pcap_if_t* dev = alldevs; dev != nullptr; dev = dev->next) {
+        ui->listBox->addItem(dev->name);
+    }
+
+    // Free the device list
+    pcap_freealldevs(alldevs);
+
+}
+void wardenfort::freeDeviceList() {
+    if (alldevs != nullptr) {
+        pcap_freealldevs(alldevs);
+        alldevs = nullptr;
+    }
+}
+
+void wardenfort::updateList(QString& text)
+{
+    // Retrieve the current list of strings from the model
+    QStringList stringList = listModel->stringList();
+
+    // Add the new text to the list
+    stringList.append(text);
+
+    // Update the model with the new list
+    listModel->setStringList(stringList);
 }
--- WardenFort/WardenFort/wardenfort.h
+++ WardenFort/WardenFort/wardenfort.h
@@ -2,6 +2,8 @@
 #define WARDENFORT_H
 
 #include <QMainWindow>
+#include <QStringListModel>
+#include <pcap.h>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -16,8 +18,66 @@ class wardenfort : public QMainWindow
 public:
     wardenfort(QWidget *parent = nullptr);
     ~wardenfort();
-
+private slots:
+    void on_LineEdit_ReturnPressed();
+    void updateList(QString& text);
+    void on_listBox_currentIndexChanged(int index);
 private:
-    Ui::wardenfort *ui;
+    Ui::wardenfort* ui;
+    
+    QStringListModel *listModel;
+    pcap_if_t* alldevs = nullptr;
+    pcap_t* adhandle = nullptr;
+    char errbuf[PCAP_ERRBUF_SIZE]; // Buffer for error messages
+    struct bpf_program fcode; // BPF program structure for packet filter
+    u_int netmask; // Network mask
+
+    u_int ip_len; // IP packet length
+    u_short sport, dport; // Source and destination ports
+
+    // Define IP address structure
+    /* Define IP header structure */
+    typedef struct ip_address {
+        u_char byte1;
+        u_char byte2;
+        u_char byte3;
+        u_char byte4;
+    } ip_address;
+
+    /* Define IPv4 header structure */
+    typedef struct ip_header {
+        u_char  ver_ihl;            // Version (4 bits) + IP header length (4 bits)
+        u_char  tos;                // Type of service 
+        u_short tlen;               // Total length 
+        u_short identification;     // Identification
+        u_short flags_fo;           // Flags (3 bits) + Fragment offset (13 bits)
+        u_char  ttl;                // Time to live
+        u_char  proto;              // Protocol
+        u_short crc;                // Header checksum
+        ip_address  saddr;          // Source address
+        ip_address  daddr;          // Destination address
+        u_int  op_pad;              // Option + Padding
+    } ip_header;
+
+    /* Define UDP header structure */
+    typedef struct udp_header {
+        u_short sport;  // Source port
+        u_short dport;  // Destination port
+        u_short len;    // Datagram length
+        u_short crc;    // Checksum
+    } udp_header;
+
+
+
+    ip_header* ih; // IP header pointer
+    pcap_if_t* dev;
+    udp_header* uh; // UDP header pointer
+
+
+    // Other member functions
+    void initialize_Devices();
+    void set_LineEdit_Text();
+    bool loadNpcapDlls();
+    void freeDeviceList();
 };
 #endif // WARDENFORT_H
